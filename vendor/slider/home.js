f3002f28cf450c6daa73f3cc624330d2073ac507
Bang Dao <daotranbang@gmail.com>
2023-05-30 08:01:42 +0700

    auto rotate slide

--- vendor/slider/home.js
+++ vendor/slider/home.js
@@ -19,13 +19,13 @@
         boxCols: 8, // For box animations
         boxRows: 4, // For box animations
         animSpeed: 500, // Slide transition speed
-        pauseTime: 3000, // How long each slide will show
+        pauseTime: 30000, // How long each slide will show
         startSlide: 0, // Set starting Slide (0 index)
         directionNav: true, // Next & Prev navigation
         controlNav: true, // 1,2,3... navigation
         controlNavThumbs: false, // Use thumbnails for Control Nav
         pauseOnHover: true, // Stop animation while hovering
-        manualAdvance: true, // Force manual transitions
+        manualAdvance: false, // Force manual transitions
         prevText: 'Prev', // Prev directionNav text
         nextText: 'Next', // Next directionNav text
         randomStart: false, // Start on a random slide
