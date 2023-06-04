3cf386cfa8e404fa657a701e05c23eb013ded1f9
son-panalogylab <son@panalogy-lab.com>
2023-06-04 12:04:38 +1000

    use new mobile logo image

--- js/main.js
+++ js/main.js
@@ -126,7 +126,7 @@
     /*-------------------------------------
      jQuery MeanMenu activation code
      --------------------------------------*/
-    $('nav#dropdown').meanmenu({ siteLogo: "<a href='index.html' class='logo-mobile'><img src='img/logo_300x98_thick_blue.svg' alt='logo' width='150'></a>" });
+    $('nav#dropdown').meanmenu({ siteLogo: "<a href='index.html' class='logo-mobile'><img src='img/logo_150x49_thick_blue.svg' alt='logo'></a>" });
     /*-------------------------------------
      Wow js Active
     -------------------------------------*/
